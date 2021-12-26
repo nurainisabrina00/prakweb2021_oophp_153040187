974266873a6a049179923c6e63ffbc8d569183e1
nurainisabrina00 <nurainisabrina@mail.unpas.ac.id>
2021-12-26 21:35:00 +0700

    ini adalah class mobil dengan fungsi kecepatan, transmisi, belok

---
+++ Mobil.php
@@ -0,0 +1,29 @@
+<?php
+
+class Mobil{
+ public $nama,
+ public $kecepatan,
+ public $warna,
+ public $kecepatanMaksimal,
+ public $jumlahPenumpang;
+
+	public function tambahKecepatan(){
+		
+	}
+
+	public function kurangiKecepatan(){
+
+	}
+
+	public function gantiTransmisi(){
+
+	}
+
+	public function belokKiri(){
+
+	}
+
+	public function belokKanan(){
+		
+	}
+}
